9c91307c7955226e5657511356124a07a0b3b905
Benji021 <9548holbertonstudents.com>
2025-05-27 10:29:56 +0200

    Advanced types Part 1

---
+++ TypeScript/task_2/js/main.ts
@@ -0,0 +1,51 @@
+// Interface Director
+interface DirectorInterface {
+    workFromHome(): string;
+    getCoffeeBreak(): string;
+    workDirectorTasks(): string;
+}
+
+// Interface Teacher
+interface TeacherInterface {
+    workFromHome(): string;
+    getCoffeeBreak(): string;
+    workTeacherTasks(): string;
+}
+
+// Class Director
+class Director implements DirectorInterface {
+    workFromHome(): string {
+        return 'Wroking from home';
+    }
+
+    getCoffeeBreak(): string {
+        return 'Getting a coffee break';
+    }
+
+    workDirectorTasks(): string {
+        return 'Cannot work from home';
+    }
+}
+
+// Class Teacher
+class Teacher implements TeacherInterface {
+    workFromeHome(): string {
+        return 'Cannot work from hom';
+    }
+
+    getCoffeeBreak(): string {
+        return 'Cannot have a break';
+    }
+
+    workTeacherTasks(): string {
+        return 'Getting to work';
+    }
+}
+
+// Function
+function createEmployee(salary: number | string): Director | Teacher {
+    if (typeof salary === 'number' && salary < 500) {
+        return new Teacher();
+    }
+    return new Director();
+}
